cb234ca1aeaebda72ca9ea3cd5ece24a35a6d593
vishal <vishal.patel929@gmail.com>
2020-06-07 16:45:34 -0400

    Got more functionality in, almost done writing ALL logic Maybe will handle errors later

--- meetingPlanner.py
+++ meetingPlanner.py
@@ -7,10 +7,10 @@ class User:
         self.allAvailable=False
         self.allUnavailable=True
     
-    def setAllAvailable():
+    def setAllAvailable(self):
         self.allAvailable=True
 
-    def setAllUnavailable():
+    def setAllUnavailable(self):
         self.allUnavailable=True
 #Defining a graph type class for my dates
 #basically the user will enter people and the dates that they cannot make it
@@ -18,23 +18,103 @@ class User:
 #if there are as many absences as dates in the range, then we will choose the dates with the least people absent
 #otherwise we will choose the dates with nobody absent
 class DateGraph:
-    def _init_():
+    def _init_(self):
+        #this dictionary maps dates to users
         self.dates={}
+        #this dictionary maps dates to their degree (we will increment this on every add)
+        self.dateDegree={}
+        #list of users that are all absent
+        self.completelyAbsent=[]
+        #list of users that are all present
+        self.completelyPresent=[]
     
     #adding a specific node based on user-timedate key pair
-    def addUser(user,timedate):
+    def addUserDate(self,user,timedate):
         if (timedate in self.dates):
             self.dates[timedate].append(user)
+            self.dateDegree[timedate]++
         else :
             self.dates[timedate]=[user]
+            self.dateDegree[timedate]=0
+
+    #adding a user that is completely absent
+    def addAbsentUser(self,user):
+        self.completelyAbsent.append(user)
+
+    #adding a user that is completely present
+    def addPresentUser(self,user):
+        self.completelyPresent.append(user)
+    
+    #getting all the users that are absent on a specific date
+    def getAbsentUsers(self,timedate):
+        absentees=[]
+        for x in self.dates:
+            absentees.append(x)
+        for x in self.completelyAbsent:
+            absentees.append(x)
+        return absentees
+
     #counts the degree of a node
-    def countDegree(timedate):
+    def countDegree(self,timedate):
         #count the degree here
+        #we just count the number of users associated to this date
+        count=0
+        for person in self.dates[timedate]:
+            count++
+        return count
 
-    def getBestDates():
+    #method to get the best dates in the graph
+    def getBestDates(self):
         #goes through all the dates and gets a list of the best dates (based on least people missing)
+        #i do one pass to find the date with the least people absent (least degree)
+        #then i do another pass to add other dates with the same degree and add them to the list
+        bestDate=None
+        bestDates=[]
+        for x in self.dateDegree:
+            if (bestDate==None):
+                bestDate=x
+            else :
+                if (self.dateDegree[bestDate]>x):
+                    #then x has the least people absent
+                    bestDate=x
+        
+        #second pass to add all the dates with this least associativity
+        for x in self.dateDegree:
+            if (self.dateDegree[bestDate]==self.dateDegree[x]):
+                bestDates.append(x)
+        #returning the list of best dates, ready to be printed out to the user
+        return bestDates
 
+    #function to print out the list of best dates and the users attending
+    def printBestDates(self,listDates):
+            if (len(listDates)>1):
+                print("Best Dates and Absentees:\n")
+                print("------------------------------------------------------\n")
+            else :
+                print("Best Date and Absentees:\n")
+                print("------------------------------------------------------\n")
+            
+            #now iterating through the dates given and printing out the date and the absentees
+            for x in listDates:
+                x.strftime("%b/%d/%Y\n")
+                print("Absent:")
+                for y in self.dates[x]:
+                    print(" "+y)
+                print("\n")
+                print("------------------------------------------------------\n")
+        
 #some functions to help with date stuff
+
+#gets all the dates in the given range (returns a list of datetimes)
+def getDateRange(begDate,endDate):
+    testDate=begDate
+    dates=[]
+    while(testDate <=endDate):
+       dates.append(testDate)
+       testDate=testDate+datetime.timedelta(day=1)
+    return dates
+
+
 
 #function to check if a date is in the range specified by the user
 def isValidDate(enteredDate, begDate,endDate):
@@ -44,11 +124,10 @@ def isValidDate(enteredDate, begDate,endDate):
         return False
 
 
-#function to take list of dates and get list of datetimes
-def getListTimes(dateString):
-    seperatedDates=dateString.split(",")
-    for 
-
+#function to turn a string in form of "mm/dd/yy" into a datetime
+def getDateTime(enteredDate):
+    date=datetime.date(enteredDate[6,9],enteredDate[0,1],enteredDate[3,4])
+    return date
 
 
 #when user is entering data about other users, they can optionally say ALL ABSENT Except ..., ALL ABSENT, NOT ABSENT, NOT ABSENT except..., in addition to just listing dates
@@ -56,6 +135,8 @@ def getListTimes(dateString):
 #likewise, if a user is not absent, they can also be excluded from the graph
 def main():
     while True
+    #creating a graph to use
+    graph = DateGraph()
     dateRange = input("Please enter a date range in the format mm/dd/yyyy:mm/dd/yyyy\n")
     #removing whitespace from the entire date, if any
     dateRange.replace(" ","")
@@ -64,13 +145,10 @@ def main():
     begDate=dateRange[0]
     endDate=dateRange[1]
     #turning our date strings into actual date objects for future usefulness
-    begDate=datetime.date(begDate[6,9],begDate[0,1],begDate[3,4])
-    #asking user to enter people attending and their availability
-    usersDatesDictionary={}
-    #maps users names to another dictionary
-    usersNamesDictionary={}
-    #count for users
-    userCount=0
+    begDate=getDateTime(begDate)
+    endDate=getDateTime(endDate)
+    #list of all usernames entered in this operation
+    users=[]
     print("Please enter the name of a user, then enter their availability in the form ALL ABSENT, NOT ABSENT, ALL ABSENT EXCEPT mm/dd/yyyy,mm/dd/yyyy...,NOT ABSENT EXCEPT mm/dd/yyyy,mm/dd/yyyy..., or just enter dates seperated by a comma. Enter as many users as you wish, when you are finished enter a -.\n")
     while True
         user=input("please enter the person's name. Enter - if you want to stop.")
@@ -78,18 +156,18 @@ def main():
         if (user=="-"):
             break
         else :
-            toAdd = User(user)
+            users.append(user)
         while True
             dates=input("Please enter either ALL ABSENT, ALL PRESENT, a date range of the form mm/dd/yyyy:mm/dd/yyyy, or an individual date of the form mm/dd/yyyy and hit enter. When you wish to stop please enter - .")
             dates.replace(" ","")
             dates.lower()
-            if (dates==allabsent):
+            if (dates=="allabsent"):
                 #we have to check off all absent
-                toAdd.setAllUnavailable()
+                graph.addCompletelyAbsentUser(user)
                 break
-            elif (dates==allpresent):
+            elif (dates=="allpresent"):
                 #we have to check off all present
-                toAdd.setAllAvailable()
+                #in other words, do nothing, because all present doesnt really matter
                 break
             elif (dates[10]==":"):
                 #we have a date range
@@ -97,10 +175,14 @@ def main():
                 dates=dates.split(":")
                 start = dates[0]
                 end = dates[1]
-                start = datetime.date(start[6,9],start[0,1],start[3,4])
-                end=datetime.date(end[6,9],end[0,1],end[3,4])
+                start = getDateTime(start)
+                end=getDateTime(end)
                 if (isValidDate(start,begDate,endDate) and isValidDate(end,begDate,endDate)):
                     #then i need to add all the dates in this range to the dictionary
+                    datesList = getDateRange(begDate,endDate)
+                    for x in datesList:
+                        #I need to add the date user pair to the graph
+                        graph.addDateUser(user,x)
             else :
                 #then we have an individual date
                 #we should check if this date is valid and then if valid, we add it, else not
